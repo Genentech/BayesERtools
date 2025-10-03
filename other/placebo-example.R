4025d15a93b80cfde04e9ee42a419fbe04fb5012
Danielle Navarro <djnavarro@protonmail.com>
2025-10-03 14:44:56 +1000

    updates examples

--- other/placebo-example.R
+++ other/placebo-example.R
@@ -1,32 +1,105 @@
 library(BayesERtools)
 
+# linear regression, no placebo
+mod <- d_sim_emax |> 
+  dev_ermod_lin(
+    var_resp = "response_1",
+    var_exposure = "exposure_1",
+    options_placebo_handling = list(include_placebo = FALSE)
+  ) 
+mod |> 
+  plot_er(
+    show_orig_data = TRUE, 
+    options_orig_data = list(var_group = "dose")
+  )
+
+# linear regression, with placebo
+mod <- d_sim_emax |> 
+  dev_ermod_lin(
+    var_resp = "response_1",
+    var_exposure = "exposure_1",
+    options_placebo_handling = list(include_placebo = TRUE)
+  ) 
+mod |> 
+  plot_er(
+    show_orig_data = TRUE, 
+    options_orig_data = list(var_group = "dose")
+  )
+
 # logistic regression, no placebo
-d_sim_emax |> 
+mod <- d_sim_emax |> 
   dev_ermod_bin(
     var_resp = "response_2",
     var_exposure = "exposure_1",
     options_placebo_handling = list(include_placebo = FALSE)
-  ) |> 
+  ) 
+mod |> 
+  plot_er(
+    show_orig_data = TRUE, 
+    options_orig_data = list(var_group = "dose")
+  )
+
+# logistic regression, with placebo
+mod <- d_sim_emax |> 
+  dev_ermod_bin(
+    var_resp = "response_2",
+    var_exposure = "exposure_1",
+    options_placebo_handling = list(include_placebo = TRUE)
+  ) 
+mod |> 
   plot_er(
     show_orig_data = TRUE, 
     options_orig_data = list(var_group = "dose")
   )
 
 
-# scenario 2: model should n
-mod2 <- dat |> 
-  dev_ermod_bin(
-    var_resp = "response_2",
-    var_exposure = "exposure",
-    options_placebo_handling = list(
-      include_placebo = TRUE,
-      method = "var_placebo",
-      var_placebo = "placebo"
-    )
+# emax regression, continuous response, no placebo
+mod <- d_sim_emax |> 
+  dev_ermod_emax(
+    var_resp = "response_1",
+    var_exposure = "exposure_1",
+    options_placebo_handling = list(include_placebo = FALSE)
+  )
+mod |> 
+  plot_er(
+    show_orig_data = TRUE, 
+    options_orig_data = list(var_group = "dose")
   )
 
-mod2 |> 
-  sim_er_curve(output_type = "median_qi") |> 
+# emax regression, continuous response, with placebo
+mod <- d_sim_emax |> 
+  dev_ermod_emax(
+    var_resp = "response_1",
+    var_exposure = "exposure_1",
+    options_placebo_handling = list(include_placebo = TRUE)
+  ) 
+mod |> 
+  plot_er(
+    show_orig_data = TRUE, 
+    options_orig_data = list(var_group = "dose")
+  )
+
+# emax regression, binary response, no placebo
+mod <- d_sim_emax |> 
+  dev_ermod_bin_emax(
+    var_resp = "response_2",
+    var_exposure = "exposure_1",
+    options_placebo_handling = list(include_placebo = FALSE)
+  ) 
+mod |> 
+  plot_er(
+    show_orig_data = TRUE, 
+    options_orig_data = list(var_group = "dose")
+  )
+
+# emax regression, binary response, with placebo
+mod <- d_sim_emax |> 
+  dev_ermod_bin_emax(
+    var_resp = "response_2",
+    var_exposure = "exposure_1",
+    options_placebo_handling = list(include_placebo = TRUE)
+  ) 
+mod |> 
   plot_er(
     show_orig_data = TRUE, 
     options_orig_data = list(var_group = "dose")
