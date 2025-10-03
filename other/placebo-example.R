ba08288503c229e5d683d0d343023c73218f8bee
Danielle Navarro <djnavarro@protonmail.com>
2025-10-03 20:39:22 +1000

    adds to the examples

--- other/placebo-example.R
+++ other/placebo-example.R
@@ -1,106 +1,173 @@
 library(BayesERtools)
 
-# linear regression, no placebo
-mod <- d_sim_emax |> 
-  dev_ermod_lin(
-    var_resp = "response_1",
-    var_exposure = "exposure_1",
-    options_placebo_handling = list(include_placebo = FALSE)
-  ) 
-mod |> 
-  plot_er(
-    show_orig_data = TRUE, 
-    options_orig_data = list(var_group = "dose")
-  )
 
-# linear regression, with placebo
-mod <- d_sim_emax |> 
-  dev_ermod_lin(
-    var_resp = "response_1",
-    var_exposure = "exposure_1",
-    options_placebo_handling = list(include_placebo = TRUE)
-  ) 
-mod |> 
-  plot_er(
-    show_orig_data = TRUE, 
-    options_orig_data = list(var_group = "dose")
-  )
+# fit ER models --------------------------------------------
 
-# logistic regression, no placebo
-mod <- d_sim_emax |> 
-  dev_ermod_bin(
-    var_resp = "response_2",
-    var_exposure = "exposure_1",
-    options_placebo_handling = list(include_placebo = FALSE)
-  ) 
-mod |> 
-  plot_er(
-    show_orig_data = TRUE, 
-    options_orig_data = list(var_group = "dose")
-  )
+fit_ermods <- list(
 
-# logistic regression, with placebo
-mod <- d_sim_emax |> 
-  dev_ermod_bin(
-    var_resp = "response_2",
-    var_exposure = "exposure_1",
-    options_placebo_handling = list(include_placebo = TRUE)
-  ) 
-mod |> 
-  plot_er(
-    show_orig_data = TRUE, 
-    options_orig_data = list(var_group = "dose")
-  )
+  # linear regression
+  lin_drop = d_sim_emax |> 
+    dev_ermod_lin(
+      var_resp = "response_1",
+      var_exposure = "exposure_1",
+      options_placebo_handling = list(include_placebo = FALSE)
+    ),
+  lin_keep = d_sim_emax |> 
+    dev_ermod_lin(
+      var_resp = "response_1",
+      var_exposure = "exposure_1",
+      options_placebo_handling = list(include_placebo = TRUE)
+    ),   
+
+  # logistic regression
+  bin_drop = d_sim_emax |> 
+    dev_ermod_bin(
+      var_resp = "response_2",
+      var_exposure = "exposure_1",
+      options_placebo_handling = list(include_placebo = FALSE)
+    ),
+  bin_keep = d_sim_emax |> 
+    dev_ermod_bin(
+      var_resp = "response_2",
+      var_exposure = "exposure_1",
+      options_placebo_handling = list(include_placebo = TRUE)
+    ),
+    
+  # emax continuous
+  emax_drop = suppressWarnings(d_sim_emax |> 
+    dev_ermod_emax(
+      var_resp = "response_1",
+      var_exposure = "exposure_1",
+      options_placebo_handling = list(include_placebo = FALSE),
+      chains = 2,
+      iter = 500
+    )),
+  emax_keep = suppressWarnings(d_sim_emax |> 
+    dev_ermod_emax(
+      var_resp = "response_1",
+      var_exposure = "exposure_1",
+      options_placebo_handling = list(include_placebo = TRUE),
+      chains = 2,
+      iter = 500,
+  )),
+
+  # emax binary
+  emax_bin_drop = suppressWarnings(d_sim_emax |> 
+    dev_ermod_bin_emax(
+      var_resp = "response_2",
+      var_exposure = "exposure_1",
+      options_placebo_handling = list(include_placebo = FALSE),
+      chains = 2,
+      iter = 500
+    )),
+  emax_bin_keep = suppressWarnings(d_sim_emax |> 
+    dev_ermod_bin_emax(
+      var_resp = "response_2",
+      var_exposure = "exposure_1",
+      options_placebo_handling = list(include_placebo = FALSE),
+      chains = 2,
+      iter = 500
+    ))
+
+)
+
+# plot ER models --------------------------------------------
+
+plt_ermods <- fit_ermods |> 
+  purrr::imap(\(m, idx) {
+    m |> 
+      plot_er(
+        show_orig_data = TRUE, 
+        options_orig_data = list(var_group = "dose")
+      ) + 
+      ggplot2::labs(title = idx)
+  })
+
+for(p in plt_ermods) print(p)
 
 
-# emax regression, continuous response, no placebo
-mod <- d_sim_emax |> 
-  dev_ermod_emax(
-    var_resp = "response_1",
-    var_exposure = "exposure_1",
-    options_placebo_handling = list(include_placebo = FALSE)
-  )
-mod |> 
-  plot_er(
-    show_orig_data = TRUE, 
-    options_orig_data = list(var_group = "dose")
-  )
+# fit expsel models ----------------------------------------
 
-# emax regression, continuous response, with placebo
-mod <- d_sim_emax |> 
-  dev_ermod_emax(
-    var_resp = "response_1",
-    var_exposure = "exposure_1",
-    options_placebo_handling = list(include_placebo = TRUE)
-  ) 
-mod |> 
-  plot_er(
-    show_orig_data = TRUE, 
-    options_orig_data = list(var_group = "dose")
-  )
+fit_expsel <- list(
 
-# emax regression, binary response, no placebo
-mod <- d_sim_emax |> 
-  dev_ermod_bin_emax(
-    var_resp = "response_2",
-    var_exposure = "exposure_1",
-    options_placebo_handling = list(include_placebo = FALSE)
-  ) 
-mod |> 
-  plot_er(
-    show_orig_data = TRUE, 
-    options_orig_data = list(var_group = "dose")
-  )
+  # linear regression
+  lin_drop = d_sim_emax |> 
+    dev_ermod_lin_exp_sel(
+      var_resp = "response_1",
+      var_exp_candidates = c("exposure_1", "exposure_2"),
+      options_placebo_handling = list(include_placebo = FALSE)
+    ),
+  lin_keep = d_sim_emax |> 
+    dev_ermod_lin_exp_sel(
+      var_resp = "response_1",
+      var_exp_candidates = c("exposure_1", "exposure_2"),
+      options_placebo_handling = list(include_placebo = TRUE)
+    ),   
 
-# emax regression, binary response, with placebo
-mod <- d_sim_emax |> 
-  dev_ermod_bin_emax(
-    var_resp = "response_2",
-    var_exposure = "exposure_1",
-    options_placebo_handling = list(include_placebo = TRUE)
-  ) 
-mod |> 
-  plot_er(
-    show_orig_data = TRUE, 
-    options_orig_data = list(var_group = "dose")
-  )
+  # logistic regression
+  bin_drop = d_sim_emax |> 
+    dev_ermod_bin_exp_sel(
+      var_resp = "response_2",
+      var_exp_candidates = c("exposure_1", "exposure_2"),
+      options_placebo_handling = list(include_placebo = FALSE)
+    ),
+  bin_keep = d_sim_emax |> 
+    dev_ermod_bin_exp_sel(
+      var_resp = "response_2",
+      var_exp_candidates = c("exposure_1", "exposure_2"),
+      options_placebo_handling = list(include_placebo = TRUE)
+    ),
+    
+  # emax continuous
+  emax_drop = suppressWarnings(d_sim_emax |> 
+    dev_ermod_emax_exp_sel(
+      var_resp = "response_1",
+      var_exp_candidates = c("exposure_1", "exposure_2"),
+      options_placebo_handling = list(include_placebo = FALSE),
+      chains = 2,
+      iter = 500
+    )),
+  emax_keep = suppressWarnings(d_sim_emax |> 
+    dev_ermod_emax_exp_sel(
+      var_resp = "response_1",
+      var_exp_candidates = c("exposure_1", "exposure_2"),
+      options_placebo_handling = list(include_placebo = TRUE),
+      chains = 2,
+      iter = 500,
+  )),
+
+  # emax binary
+  emax_bin_drop = suppressWarnings(d_sim_emax |> 
+    dev_ermod_bin_emax_exp_sel(
+      var_resp = "response_2",
+      var_exp_candidates = c("exposure_1", "exposure_2"),
+      options_placebo_handling = list(include_placebo = FALSE),
+      chains = 2,
+      iter = 500
+    )),
+  emax_bin_keep = suppressWarnings(d_sim_emax |> 
+    dev_ermod_bin_emax_exp_sel(
+      var_resp = "response_2",
+      var_exp_candidates = c("exposure_1", "exposure_2"),
+      options_placebo_handling = list(include_placebo = FALSE),
+      chains = 2,
+      iter = 500
+    ))
+
+)
+
+# plot expsel models --------------------------------------------
+
+plt_expsel <- fit_expsel |> 
+  purrr::imap(\(m, idx) {
+    m |> 
+      plot_er(
+        show_orig_data = TRUE, 
+        options_orig_data = list(var_group = "dose")
+      ) + 
+      ggplot2::labs(title = idx)
+  })
+
+for(p in plt_expsel) print(p)
+
+
