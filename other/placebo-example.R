f92cddc79b9d549d5bc836c06905eadeb761d5e8
Danielle Navarro <djnavarro@protonmail.com>
2025-10-03 11:41:13 +1000

    strips back example code

--- other/placebo-example.R
+++ other/placebo-example.R
@@ -1,22 +1,12 @@
 library(BayesERtools)
 
-# a data set that includes a placebo group
-dat <- dplyr::bind_rows(
-  readr::read_csv("data-raw/d_sim_emax.csv"), # current emax data set in pacakge
-  readr::read_csv("other/d_sim_emax_placebo.csv") # placebo data from same model
-) |> 
-  dplyr::mutate(placebo = dose == 0) # represent the placebo group explicitly
-
-# scenario 1: default is to implicitly drop placebo, assuming that the placebo
-# group maps onto the zero exposure rows
-mod1 <- dat |> 
+# logistic regression, no placebo
+d_sim_emax |> 
   dev_ermod_bin(
     var_resp = "response_2",
-    var_exposure = "exposure"
-  )
-
-mod1 |> 
-  sim_er_curve(output_type = "median_qi") |> 
+    var_exposure = "exposure_1",
+    options_placebo_handling = list(include_placebo = FALSE)
+  ) |> 
   plot_er(
     show_orig_data = TRUE, 
     options_orig_data = list(var_group = "dose")
